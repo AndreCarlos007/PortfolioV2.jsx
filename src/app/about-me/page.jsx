a72c4954467e5e76ac8a8f6a053aba8c48435cbe
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-17 12:00:12 -0300

    :sparkles: feat: Page send an email criada e ja estilizada, lib emailjs instalado e configurada de forma funcional

--- src/app/about-me/page.jsx
+++ src/app/about-me/page.jsx
@@ -102,6 +102,50 @@ const page = () => {
                     </div>
                 </span>
                 </section>
+
+                <span className="" style={{ opacity: 1, transform: "none" }}>
+              <div className="flex flex-col gap-6 mt-6">
+                <div className="relative group flex size-full rounded-md p-6 w-full z-10 hover:shadow-xl">
+                  <div className="absolute inset-px z-10 rounded-md bg-colorCard  transition-colors"></div>
+                  <div className="relative z-30 w-full">
+                    <div className="flex flex-col gap-4 h-full">
+                      <div className="flex flex-col gap-1">
+                        <div className="p-2 rounded-lg bg-[#262626] w-fit">
+                          <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            strokeWidth="0"
+                            viewBox="0 0 448 512"
+                            height="12"
+                            width="12"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
+                          </svg>
+                        </div>
+                        <div className="flex items-center gap-2 ">
+                          <p className="text-lg font-semibold">Email</p>
+                        </div>
+                        <p className="text-sm opacity-55">
+                          {" "}
+                          Send an email for me
+                        </p>
+                      </div>
+                      <Link href="/send-email">
+                        <button className="inline-flex items-center justify-center rounded-md text-white/60 hover:text-white font-medium transition-colors  border border-colorBorder hover:bg-colorBorder text-xs px-[8px] py-[6px] gap-2">
+                          View email
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                  <div
+                    className="pointer-events-none absolute inset-px z-10 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100 hidden md:block"
+                    style={{ opacity: 0.7 }}
+                  ></div>
+                  <div className="pointer-events-none absolute inset-0 rounded-md bg-colorCard border border-colorBorder duration-300 group-hover:opacity-100 hidden md:block"></div>
+                </div>
+              </div>
+            </span>
             </div>
         </main>
       </div>
--- src/app/send-email/page.jsx
+++ src/app/send-email/page.jsx
@@ -1,9 +1,133 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const page = () => {
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [statusMessage, setStatusMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false); // Estado para controlar se o envio está em andamento
+
+  // Função para lidar com mudanças nos inputs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Função para verificar se todos os campos estão preenchidos
+  const isFormValid = () => {
+    return Object.values(formData).every((value) => value.trim() !== "");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSubmitting) return; // Impede o envio múltiplo
+
+    setIsSubmitting(true); // Marca o início do envio
+
+    const serviceId = "service_oi4yvzs";
+    const templateId = "template_il7i00m";
+    const userId = "1JfgqlqaOWmql2M2_";
+
+    emailjs
+      .send(serviceId, templateId, formData, userId)
+      .then(() => {
+        setStatusMessage("Message sent successfully!");
+        setFormData({ name: "", email: "", subject: "", message: "" });
+      })
+      .catch((error) => {
+        setStatusMessage("Error sending message. Please try again.");
+        console.error("EmailJS error:", error);
+      })
+      .finally(() => {
+        setIsSubmitting(false); // Marca o fim do envio
+      });
+  };
+
+
   return (
     <div>
-      send an email
+      <div className="flex-1 flex flex-col">
+        <main className="w-full flex justify-center pb-[7.5rem] py-[2.5rem] mx-auto md:my-[3.125rem] px-8 md:pb-0 md:max-w-4xl">
+            <div className='flex flex-col'>
+                <div className='flex flex-col md:flex-row items-center gap-4'>
+                    <div className='w-full'>
+                    <span className="" style={{ opacity: 1, transform: "none" }}>
+                        <div className='flex flex-col gap-2'>
+                        <h1 className=" text-2xl md:text-4xl font-semibold leading-10 text-pretty">Send an Email</h1>
+                        <p className='opacity-60'>
+                        Contact me by email!
+                        </p>
+                        </div>
+                    </span>
+                    </div>
+                </div>
+                <span className="" style={{ opacity: 1, transform: "none" }}>
+            <div className="flex flex-col gap-6 mt-6">
+                  <div className="relative group flex size-full rounded-md p-6 w-full z-10 hover:shadow-xl">
+                    <div className="absolute inset-px z-10 rounded-md bg-colorCard  transition-colors"></div>
+                    <div className="relative z-30 w-full">
+                      
+                      <form onSubmit={handleSubmit}  className='flex flex-col gap-4 h-full'>
+                        <div className="flex flex-col gap-1">
+                        
+                          <div className="flex flex-row justify-around p-2 gap-5 ">
+                          
+                            <div className='flex w-full gap-5'>
+                            <input  name="name"
+                            value={formData.name}
+                            onChange={handleInputChange} placeholder='Name' type="text" className='placeholder:text-white/60 text-white pl-2 capitalize  outline-none w-full bg-[#262626] p-2 rounded-lg' />
+                            
+                            <input name="email"
+                            value={formData.email}
+                            onChange={handleInputChange} placeholder='Email' type="email" className='placeholder:text-white/60 text-white pl-2 outline-none w-full bg-[#262626] p-2 rounded-lg' />
+                            </div>
+                            
+                          </div>
+                          <div className="flex items-center gap-2">
+                            <input 
+                            name="subject"
+                            value={formData.subject}
+                            onChange={handleInputChange} placeholder='Subject' type="text" className='placeholder:text-white/60 text-white pl-2 w-full mx-2  outline-none bg-[#262626] p-2 rounded-lg'/>
+                          </div>
+                          
+                        </div>
+                        
+                        <textarea value={formData.message}
+                        onChange={handleInputChange} placeholder='Message' name="message" id="" className='placeholder:text-white/60 text-white pl-2  mx-2 outline-none bg-[#262626] p-2 rounded-lg'></textarea>
+                        
+                        <button type='submit' className={`bg-[#262626] text-white/60 hover:text-white p-2 
+                          rounded-lg w-16 mx-2 hover:bg-colorCard border border-colorBorder hover:border-colorBorder 
+                          transition-all duration-300 ${isFormValid() && !isSubmitting ? "" : "opacity-10 cursor-not-allowed text-opacity-0"}`}
+                          disabled={!isFormValid() || isSubmitting }>Send</button>
+                      </form>
+                      
+                      
+                      
+                    </div>
+                    
+                    <div
+                      className="pointer-events-none absolute inset-px z-10 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100 hidden md:block"
+                      style={{ opacity: 0.7 }}
+                    ></div>
+                    <div className="pointer-events-none absolute inset-0 rounded-md bg-colorCard border border-colorBorder duration-300 group-hover:opacity-100 hidden md:block"></div>
+                  </div>
+                </div>
+            </span>
+            {statusMessage && <p className="text-white/60 mt-4 text-center">{statusMessage}</p>}
+            </div>
+        </main>
+      </div>
     </div>
   )
 }
